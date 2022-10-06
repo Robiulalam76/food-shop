96ea1b30b606fc05430787a4382bb3a3309e8fe7
robiulalam76 <robiulalam762400@gmail.com>
2022-10-06 23:24:12 +0600

    code last update

--- src/Components/Food/Food.js
+++ src/Components/Food/Food.js
@@ -5,15 +5,15 @@ const Food = ({ food }) => {
     const { idMeal, strInstructions, strMealThumb, strMeal } = food
     // console.log(food)
     return (
-        <div className='mx-auto'>
-            <div className="card w-96 bg-base-100 shadow-xl bg-[#2A303C] mx-auto">
+        <div className='w-[95%] mx-auto'>
+            <div className="card h-full w-full shadow-xl bg-[#2A303C]">
                 <figure><img className='h-44 w-full' src={strMealThumb} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title text-white">
                         {strMeal}
                         <div className="badge badge-secondary">NEW</div>
                     </h2>
-                    <p className='text-gray-300'>{strInstructions ? strInstructions.slice(0, 150) : 'No Data Found'}</p>
+                    <p className='text-gray-300'>{strInstructions ? strInstructions.slice(0, 120) + '...' : 'No Data Found'}</p>
                     <div className="card-actions justify-end">
                         <Link to={`/food-details/${idMeal}`} className='btn btn-primary'>Details</Link>
                     </div>
